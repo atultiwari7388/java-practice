1a176cfb3ed7f80750067be897da352a4e4136f0
Atul Tiwari <tiwariatul9526@gmail.com>
2022-10-15 14:43:09 +0530

    add two numbers

--- firstexample.java
+++ firstexample.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 class FirstExample {
 
+  // Take 2 variables 'a' & 'b' and print their sum
+
   public static void main(String[] args) {
     try (Scanner sc = new Scanner(System.in)) {
       int a = sc.nextInt();
