54a686d5c6c7d760be6290776f1737c1f0fa092e
Atul Tiwari <tiwariatul9526@gmail.com>
2022-10-15 16:20:00 +0530

    switch case , if else

--- conditions.java
+++ conditions.java
@@ -3,7 +3,22 @@ import java.util.Scanner;
 public class conditions {
 
   public static void main(String[] args) {
+
+    // check number is odd or even
+
     Scanner sc = new Scanner(System.in);
+
+    int x = sc.nextInt();
+
+    if (x % 2 == 0) {
+      // print even number
+      System.out.println(x + " is Even Number");
+    } else {
+      // print odd number
+      System.out.println(x + " is Odd Number");
+    }
+
+    // check if age == 18 => Adult || not Adult
     int age = sc.nextInt();
 
     if (age > 18) {
@@ -11,6 +26,39 @@ public class conditions {
     } else {
       System.out.println("Not Adult");
 
+    }
+
+    // take 2 input from user and check a==b -> print('equal') && a>b -> print("a
+    // is
+    // greater") && a<b -> print(" a is lesser").
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    if (a == b) {
+      System.out.println(a + " is Equal");
+    } else if (a > b) {
+      System.out.println(a + " is Greater");
+    } else {
+      System.out.println(a + " is Lesser");
+    }
+
+    int button = sc.nextInt();
+
+    switch (button) {
+      case 1:
+        System.out.println("Hello");
+        break;
+      case 2:
+        System.out.println("Namaste");
+        break;
+      case 3:
+        System.out.println("Varakkam");
+        break;
+
+      default:
+        System.out.println("Invalid Button");
+        break;
     }
 
   }
